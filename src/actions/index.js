56024857ff197e7b1d501e1467d456c680f8e39f
AV <vihrogonov@protonmail.com>
2021-06-30 19:04:07 +0100

    Form Validation

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=somethingunique001';
@@ -17,6 +18,16 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request,
+  };
+}
+
+// Create post
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
     payload: request,
   };
 }
